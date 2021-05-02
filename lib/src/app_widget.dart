920475497f60bd1c19ad0c7e7147bfe6d2b0627e
PedroHenriqueDevBR <p.lyma18@gmail.com>
2021-05-02 19:40:31 -0300

    style: change statusbar and navigatorbar color

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,10 +1,21 @@
 import 'package:dark_notepad/src/pages/home/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:asuka/asuka.dart' as asuka;
+import 'package:flutter/services.dart';
 
 class App extends StatelessWidget {
+  void changeBarColor() {
+    SystemUiOverlayStyle appTheme = SystemUiOverlayStyle.dark.copyWith(
+      statusBarColor: Colors.blueGrey.shade900,
+      systemNavigationBarColor: Colors.blueGrey.shade900,
+      systemNavigationBarDividerColor: Colors.blueGrey.shade900,
+    );
+    SystemChrome.setSystemUIOverlayStyle(appTheme);
+  }
+
   @override
   Widget build(BuildContext context) {
+    changeBarColor();
     return MaterialApp(
       builder: asuka.builder,
       debugShowCheckedModeBanner: false,
